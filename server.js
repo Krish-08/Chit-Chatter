13060371e71cef095984b9a4f989bbc36f05164b
Krish <krishmdu02@gmail.com>
2021-06-22 16:56:58 +0530

    six  commit

--- server.js
+++ server.js
@@ -4,59 +4,62 @@ const http=require('http');
 const socketio=require('socket.io');
 const path=require('path');
 const server = http.createServer(app);
-const formatMsg=require('./utils/formatMsg.js')
+// const formatMsg=require('./utils/formatMsg.js')
 
-const {userJoin,getUser,leaveUser,getRoomuser}=require('./utils/users.js')
-const io=socketio(server);
+// const {userJoin,getUser,leaveUser,getRoomuser}=require('./utils/users.js')
+// const io=socketio(server);
 
 
-//static methods
-app.use(express.static(path.join(__dirname,"public")))
+// //static methods
+// app.use(express.static(path.join(__dirname,"public")))
 
-//connecting to client
-io.on("connection",socket=>{
-    console.log("Connection established")
-    socket.on("joinRoom",({userName,roomName})=>{
-        const user=userJoin(socket.id,userName,roomName);
-        socket.join(user.roomName);
-        socket.emit("message",formatMsg("Chit-Chatter(Admin)","Welcome to the Room"))
+// //connecting to client
+// io.on("connection",socket=>{
+//     console.log("Connection established")
+//     socket.on("joinRoom",({userName,roomName})=>{
+//         const user=userJoin(socket.id,userName,roomName);
+//         socket.join(user.roomName);
+//         socket.emit("message",formatMsg("Chit-Chatter(Admin)","Welcome to the Room"))
 
-        //client joined
-        socket.broadcast.to(user.roomName).emit("message",formatMsg("Chit-Chatter(Admin)",`${user.userName} joined the room`))
+//         //client joined
+//         socket.broadcast.to(user.roomName).emit("message",formatMsg("Chit-Chatter(Admin)",`${user.userName} joined the room`))
 
-    })
+//     })
 
     
     
-    //listening message
-    socket.on("chatMessage",message=>{
-        const user=getUser(socket.id);
+//     //listening message
+//     socket.on("chatMessage",message=>{
+//         const user=getUser(socket.id);
 
-        io.to(user.roomName).emit("message",formatMsg(`${user.userName}`,message))
-    })
+//         io.to(user.roomName).emit("message",formatMsg(`${user.userName}`,message))
+//     })
 
-    //client disconnected
-    socket.on("disconnect",()=>{
-        const user=leaveUser(socket.id);
-        if(user){
-            io.to(user.roomName).emit("message",formatMsg("Chit-Chatter(Admin)",`${user.userName} Disconnected from the room`))
-        }
+//     //client disconnected
+//     socket.on("disconnect",()=>{
+//         const user=leaveUser(socket.id);
+//         if(user){
+//             io.to(user.roomName).emit("message",formatMsg("Chit-Chatter(Admin)",`${user.userName} Disconnected from the room`))
+//         }
         
-    })
+//     })
 
-})
+// })
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"public/HomePage.html"))
-})
-app.get("/chatRoom",(req, res)=>{
-    res.sendFile(path.join(__dirname,"public/chatRoom.html"))
-})
+// app.get("/",(req,res)=>{
+//     res.sendFile(path.join(__dirname,"public/HomePage.html"))
+// })
+// app.get("/chatRoom",(req, res)=>{
+//     res.sendFile(path.join(__dirname,"public/chatRoom.html"))
+// })
 
 
 
 
 server.listen(3000||process.env.PORT,'0.0.0.0',(err) => {
     if(err)console.log(err)
+    else{
+        console.log("RUNNING")
+    }
 })
 
